21a62f2dd928305e5fe83285d01c72c0f1bb14f8
chguo1 <chguo@alaska.edu>
2022-12-16 17:35:16 -0500

    EDA plots of NFA family rel abun over time, and boxplots over time and by region

--- scripts/FishAtlas_4_diverse.R
+++ scripts/FishAtlas_4_diverse.R
@@ -16,6 +16,7 @@
 
 library(tidyverse)
 library(diverse)
+library(lubridate)
 
 # Define workflow paths ---------------------------------------------------
 
@@ -70,18 +71,117 @@ fam_abun_div.4 = bind_cols(fam_abun_q0, fam_abun_q1, fam_abun_q2) %>%
   select(colnames(.) %>% sort()) %>% # re-order the indices so same is next to same
   bind_cols(fam_abun_div.3, .)
 
+fam_abun_div.5 = fam_abun_div.4 %>%
+  mutate(VisitID = row.names(fam_abun_div.4)) %>%
+  relocate(VisitID, .before = 1)
+
 # Rename our final version of the df,
-fam_abun_diverse = fam_abun_div.4
+fam_abun_diverse = fam_abun_div.5
 
 rm(fam_abun_q0, fam_abun_q1, fam_abun_q2, remove_names,
-   fam_abun_div.1, fam_abun_div.2, fam_abun_div.3, fam_abun_div.4) # clean up
+   fam_abun_div.1, fam_abun_div.2, fam_abun_div.3, fam_abun_div.4, fam_abun_div.5) # clean up
 
 
 # diverse EDA -------------------------------------------------------------
 
-# 
+# Proportion of familers over time:
+left_join(fam_abun, select(visits_qc, VisitID, Date), by = "VisitID") %>%
+  mutate(Year = year(Date)) %>%
+  filter(Year > 1980) %>%
+  group_by(Year) %>%
+  mutate(Year_Tot_Abun = sum(Abundance),
+         Year_n_Visits = n_distinct(VisitID)) %>%
+  group_by(Year, Fam_CommonName) %>%
+  mutate(Year_Fam_Abun = sum(Abundance)) %>%
+  ungroup() %>%
+  select(Year, Fam_CommonName, Year_Fam_Abun, Year_Tot_Abun, Year_n_Visits) %>%
+  distinct() %>%
+  {
+    ggplot(data = ., aes(x = Year, y = Year_Fam_Abun/Year_Tot_Abun, fill = Fam_CommonName)) +
+      geom_col(aes(fill = Fam_CommonName)) +
+      geom_text(data = filter(., Year_Fam_Abun/Year_Tot_Abun > 0.1),
+                aes(label = round(Year_Fam_Abun/Year_Tot_Abun, 2)),
+                position = position_stack(), size = 3)
+  }
+  
+
+
+boxplot.n <- function(x){
+  return(c(y = max(x), label = length(x)))
+}
+
+# Let's start with Richness over time in years, and display it with Region groups:
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, variety)) %>%
+  mutate(Year = year(Date)) %>%
+  filter(Year > 1980) %>%
+  ggplot(data = ., aes(x = Year, y = variety, group = Year)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75)
+
+# Next, let's do Shannon's Index of diversity, same subsetting as above:
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, entropy)) %>%
+  mutate(Year = year(Date)) %>%
+  filter(Year > 1980) %>%
+  ggplot(data = ., aes(x = Year, y = entropy, group = Year)) + 
+    geom_boxplot() +
+    stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+    geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75)
+
+# Now let's try Gini-Simposon,
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, gini.simpson)) %>%
+  mutate(Year = year(Date)) %>%
+  filter(Year > 1980) %>%
+  ggplot(data = ., aes(x = Year, y = gini.simpson, group = Year)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75)
+
+# And lastly, let's try Berger-Parker,
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, berger.parker.D)) %>%
+  mutate(Year = year(Date)) %>%
+  filter(Year > 1980) %>%
+  ggplot(data = ., aes(x = Year, y = berger.parker.D, group = Year)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75)
 
 
 
+# Now let's check out Richness over month:
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, variety)) %>%
+  mutate(Month = month(Date)) %>%
+  ggplot(data = ., aes(x = Month, y = variety, group = Month)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75) +
+  scale_x_discrete(limits = month.abb)
+
+# Next, let's do Shannon's Index of diversity, same subsetting as above:
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, entropy)) %>%
+  mutate(Month = month(Date)) %>%
+  ggplot(data = ., aes(x = Month, y = entropy, group = Month)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75) +
+  scale_x_discrete(limits = month.abb)
+
+# Now let's try Gini-Simposon,
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, gini.simpson)) %>%
+  mutate(Month = month(Date)) %>%
+  ggplot(data = ., aes(x = Month, y = gini.simpson, group = Month)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75) +
+  scale_x_discrete(limits = month.abb)
+
+# And lastly, let's try Berger-Parker,
+left_join(select(visits_qc, VisitID, Date, Region), select(fam_abun_diverse, VisitID, berger.parker.D)) %>%
+  mutate(Month = month(Date)) %>%
+  ggplot(data = ., aes(x = Month, y = berger.parker.D, group = Month)) + 
+  geom_boxplot() +
+  stat_summary(fun.data = boxplot.n, geom = "text", vjust = -0.5) +
+  geom_jitter(aes(color = Region), size = 0.5, alpha = 0.75) +
+  scale_x_discrete(limits = month.abb)
 
 
