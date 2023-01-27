4932af866c8b42589ea2534753f71b3ac0f50a5c
chguo1 <chguo@alaska.edu>
2023-01-27 13:22:31 -0900

    figs for which genus have info in fishbase

--- scripts/FishAtlas_4_fishbase.R
+++ scripts/FishAtlas_4_fishbase.R
@@ -61,5 +61,37 @@ fishbase = length_weight(Genus = genus_list,
 # Save an object with the Genuses we have fishbase info for,
 fishbase$Gen_ScientificName %>% unique() %>% sort() -> genus_list_onFB
 
-
-
+# Perc freq of occurrence
+catch_qc %>%
+  group_by(VisitID, Gen_ScientificName) %>%
+  summarise(Presence = n_distinct(Gen_ScientificName)) %>%
+  ungroup() %>%
+  group_by(Gen_ScientificName) %>%
+  summarise(Occurrence = sum(Presence)) %>%
+  mutate(Perc_Occurrence = Occurrence / n_distinct(catch_qc$VisitID) * 100,
+         Gen_ScientificName = fct_reorder(as.factor(Gen_ScientificName), desc(Perc_Occurrence))) %>%
+    ggplot(aes(x = fct_reorder(as.factor(Gen_ScientificName), Perc_Occurrence), y = Perc_Occurrence)) +
+      geom_col(aes(fill = ifelse(Gen_ScientificName %in% genus_list_onFB,
+                                 "Yes",
+                                 "No"))) +
+      scale_fill_manual(values = c("#f03b20", "#999999"), name = "Is data on FishBase?", breaks = c("Yes", "No")) +
+      geom_text(aes(label = round(Perc_Occurrence, 1)), size = 2, hjust = 1.5) +
+      coord_flip(expand = FALSE) +
+      labs(x = "Genus", y = "Percent Frequency of Occurence")
+ggsave("nfa_fishbase_perc-occur.png", plot = last_plot(), device = 'png', path = file.path(dir.figs))
+  
+# Abundance
+catch_qc %>%
+  group_by(Gen_ScientificName) %>%
+  summarise(Abundance = sum(Count) %>% sqrt()) %>%
+  mutate(Gen_ScientificName = fct_reorder(as.factor(Gen_ScientificName), Abundance)) %>%
+  ggplot(aes(x = Gen_ScientificName, y = Abundance)) +
+    geom_col(aes(fill = ifelse(Gen_ScientificName %in% genus_list_onFB,
+                               "Yes",
+                               "No"))) +
+    scale_fill_manual(values = c("#f03b20", "#999999"), name = "Is data on FishBase?", breaks = c("Yes", "No")) +
+    geom_text(aes(label = round(Abundance, 0), hjust = 1)) +
+    coord_flip(expand = FALSE) +
+    labs(x = "Genus", y = "sqrt(Abundance)")
+ggsave("nfa_fishbase_sqrt-abun.png", plot = last_plot(), device = 'png', path = file.path(dir.figs))
+  
