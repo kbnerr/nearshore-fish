84ec8cb0850dea6370db15ae2d842f70ecdb48ee
chguo1 <chguo@alaska.edu>
2025-07-26 17:22:24 -0800

    ..

---
+++ FishLife.R
@@ -0,0 +1,11 @@
+# look into FishLife, https://github.com/James-Thorson-NOAA/FishLife?tab=readme-ov-file
+# as a way to assign function traits to nfaa data
+
+## Logerwell et al. 2015 - Used size as poxy for age
+# Research gap in studies that 'resolve' how different life stages use different habitats.
+
+## Re-read:
+
+# Henriques et al. 2017 for functional patterns worldwide
+
+# De Silva and Fabre 2019 for rare species removal effect on functional resiliency
--- scripts/nfaa_spatial.R
+++ scripts/nfaa_spatial.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 library(sf)
-library(smoothr)
+
+### NOTES
+# Should try to simplify shorezone nodes to a reasonable distance... how to decide
+# Fish migrate to different distances - use largest? estimate SZ unit length? subjective distance?
+# Best way to account for this is functional trait for migration range - possible with FishLife?
+
 
 # Directory
 wd = here::here()
@@ -12,6 +17,8 @@ for (i in seq_along(dirs)) {
   rm(name, path, i)
 }
 
+# Load NFAA data
+load(file = file.path(dir.data, "nfaa_2.rda"))
 
 # ADNR coastline map
 adnr = read_sf(dsn = file.path(dir.data.ignore, "adnr_ak_cst63", "adnr_ak_cst63xsi.shp")) %>%
@@ -55,19 +62,28 @@ ggplot(data = fishatlas) +
   aes(fill = Region) +
   geom_sf()
 
+# Merge the two regions into single polygon
+fishatlas = st_union(fishatlas, by_feature = FALSE)
+# Plot again to see merged polygon
+ggplot(data = fishatlas) +
+  aes() +
+  geom_sf()
 
 # Retain adnr coastline that intersects with goa regions
-goa_coast = st_intersection(adnr, fishatlas)
+goa.coast = st_intersection(adnr, fishatlas)
+
+# Don't need adnr ojbect anynmore,
+rm(adnr)
 
 # Check size
-object.size(goa_coast) %>% format(units = "MB")
+object.size(goa.coast) %>% format(units = "MB")
 
 # View a simplified map of this
-st_simplify(goa_coast, preserveTopology = TRUE, dTolerance = 100) %>%
+goa_map = st_simplify(goa.coast, preserveTopology = TRUE, dTolerance = 100) %>%
   ggplot(data = .) +
   aes() +
   geom_sf(color = "black")
-
+goa_map
 
 
 
@@ -76,34 +92,33 @@ st_simplify(goa_coast, preserveTopology = TRUE, dTolerance = 100) %>%
 # ShoreZone
 st_layers(dsn = file.path(dir.data.ignore, "shorezone.gdb"))
 
-# Create a vector with the names of the layers we want to read in
-sz.layers = st_layers(dsn = file.path(dir.data.ignore, "shorezone.gdb")) %>%
-  pull(name) %>%
-  str_subset("wXSHR")
-
-# Read in data
-shorezone = st_read(dsn = file.path(dir.data.ignore, "shorezone.gdb"), layer = sz.layers)
+# There are few layers we want...
+sz.mls = st_read(dsn = file.path(dir.data.ignore, "shorezone.gdb"), layer = "_AK_UNIT_LINES_WATTRS_1_1_1")
+sz.pts = st_read(dsn = file.path(dir.data.ignore, "shorezone.gdb"), layer = "_AK_UNIT_BOUNDS")
 
 # Remove columns
-shorezone = shorezone %>%
+sz.mls = sz.mls %>%
   select(OBJECTID_1,
+         PHY_IDENT,
+         StartX, StartY,
+         EndX, EndY,
          LENGTH_M,
          ESI,
          ORI,
          EXP_BIO,
          HabClass,
          HAB_CLASS_Name,
-         SHORETYPE,
-         ShoreType_desc)
+         Exposure,
+         SHORETYPE)
 
-# View object
-shorezone; st_crs(shorezone)
+# View line string object
+sz.mls; st_crs(sz.mls)
 
 # Same as with ADNR coastline, retain features that intersect with the GOA region
-shorezone = st_intersection(shorezone, fishatlas)
+sz.mls = st_intersection(sz.mls, fishatlas)
 
 # Check size
-object.size(shorezone) %>% format(units = "MB")
+object.size(sz.mls) %>% format(units = "MB")
 
 
 
@@ -113,10 +128,76 @@ object.size(shorezone) %>% format(units = "MB")
 # snap to nearest shorezone feature -> add shorezone fields to samples -> export as tibble
 # Use this ojbect for later analyses
 
+# Create an vector of Region names for GOA and Aleutian samples
+goa.regions = visits %>% pull(Region) %>% unique() %>% str_subset("GOA|Aleutians")
+
+# Visit coordinates as point geometry
+samples = visits %>%
+  # Filter for Gulf of Alaska samples
+  filter(Region %in% goa.regions) %>%
+  # Create geometry from lat/lon in WGS 84
+  st_as_sf(coords = c("Lon", "Lat"), crs = 4326) %>%
+  # Project to pseudo mercator
+  st_transform(crs = 3857)
+
+# Generate map of samples
+goa_map + 
+  geom_sf(data = samples, aes(color = Region), alpha = 0.6) +
+  coord_sf(expand = FALSE)
+
+# Pull ID from shorezone nearest each sample
+mls_ids = st_nearest_feature(samples, sz.mls)
+# Filter shorezone to samples
+sz.samples = filter(sz.mls, OBJECTID_1 %in% mls_ids)
+
+# Add this reduced SZ layer to our map
+goa_map + 
+  geom_sf(data = sz.samples, color = "green") +
+  coord_sf(expand = FALSE)
+
+# Issues to address: some mls samples not in AK, not all samples have associated features- which ones?
+# try other forms of st_join, like st_contains_properly, st_is_within_distance, etc
+
+
+
+
+
 # Separately, we want to explore spatial auto-correlation with a variogram:
 # Read in samples -> assign geometry -> project into pseudo mercator crs 3857;
 # Create a geospatial network with samples (as new nodes) and coastline (edges) ->
 # Try st_triangulate to create connected edges (possible routes) among nodes ->
 # Calculate distances based off of this network -> produce variogram.
+
+# Code for testing time to plot
+bmplot = function(x) {
+  plot = goa_map + 
+  geom_sf(data = x, color = "green") +
+  coord_sf(expand = FALSE)
+  return(plot)
+}
+res = microbenchmark::microbenchmark(bmplot(sz.pts), unit = "seconds")
+print(res)
+
+# Plot shorezone coverage in the GOA
+goa_map + 
+  geom_sf(data = sz.pts, color = "green", alpha = 0.2) +
+  coord_sf(expand = FALSE)
+
+# Make an sfnetwork
+library(sfnetworks)
+
+# Cast shorezone from MultiLineString to LineString
+sz.ls = st_cast(sz.mls, to = "LINESTRING")
+
+# Create sf network from shorezone edges
+net = as_sfnetwork(sz.ls, directed = FALSE)
+
+# Quick plot of network
+library(plotly)
+plot_net = autoplot(net)
+ggplotly(plot_net)
+
+
+
 
 
